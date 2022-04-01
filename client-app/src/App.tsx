6aed4525a3e4907a92208ee0357c63881b2463a9
Lance Watt <lanceawatt@hotmail.com>
2022-04-01 17:06:39 +0200

    End of Section 3, Semantic UI seems to be working.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
 
 function App() {
   const [activities, setActivities] = useState([]); // useState = React Hook
@@ -10,21 +11,21 @@ function App() {
     axios.get('http://localhost:5000/activities').then(response => {
       console.log(response);
       setActivities(response.data); // data will hold activities
-
     })
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          {activities.map((activity: any) => (
-            <li key={activity.id}>
-              {activity.title}
-            </li>
-          ))}
-        </ul>
-      </header>
+    <div>
+
+      <Header as='h2' icon='users' content='Reactivities' />
+
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
